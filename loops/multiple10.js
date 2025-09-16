fed35e841e9c9fc4c42ab2b68126b1fceb6dc410
ashish017-17 <ashish03724@gmail.com>
2025-09-16 18:12:09 +0530

    add

---
+++ loops/multiple10.js
@@ -0,0 +1,7 @@
+// let numbers = [2, 4, 6];
+// Expected: 20, 40, 60
+// Multiply each number by 10 and print the result.
+let numbers = [2, 4, 6];
+numbers.forEach(number => {
+  console.log(number * 10);
+});
